baa031f4aa687a6cf31ebc7f798e88c04b9e7406
MrWisz <eduardoluis09@hotmail.com>
2024-05-01 17:01:46 -0400

    inclusion del formulario de registro luego de solucionar problemas de merge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './pages/Login.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx'; // Import Register
 
 const router = createBrowserRouter([
-{
-  path:'/',
-  element:<App/>,
-  errorElement:<div>404</div>
-},
-{
-  path:'/login',
-  element:<Login/>,
-  errorElement:<div>404</div>
-}]);
+  {
+    path:'/',
+    element:<App/>,
+    errorElement:<div>404</div>
+  },
+  {
+    path:'/login',
+    element:<Login/>,
+    errorElement:<div>404</div>
+  },
+  {
+    path:'/register', // Add a new route for Register
+    element:<Register/>,
+    errorElement:<div>404</div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
