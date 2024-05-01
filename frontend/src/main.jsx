05b2d6d28a157935374f06c7d5d34e50a2900d65
MrWisz <eduardoluis09@hotmail.com>
2024-05-01 17:10:48 -0400

    commit de backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx'; // Import Register
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './pages/Login.jsx'
-import TestNutricional from './pages/TestNutricional.jsx'
+import Register from './pages/Register.jsx';
+import TestNutricional from './pages/TestNutricional.jsx';
 
 const router = createBrowserRouter([
   {
@@ -27,29 +19,19 @@ const router = createBrowserRouter([
     errorElement:<div>404</div>
   },
   {
-    path:'/register', // Add a new route for Register
+    path:'/register',
     element:<Register/>,
+    errorElement:<div>404</div>
+  },
+  {
+    path:'/testnutricional',
+    element:<TestNutricional/>,
     errorElement:<div>404</div>
   }
 ]);
-{
-  path:'/',
-  element:<App/>,
-  errorElement:<div>404</div>
-},
-{
-  path:'/login',
-  element:<Login/>,
-  errorElement:<div>404</div>
-},
-{
-  path:'/testnutricional',
-  element:<TestNutricional/>,
-  errorElement:<div>404</div>
-}]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
